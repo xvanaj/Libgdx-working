6938dbc53d26ea0eafec7d8b9e89ed9cc95d25b8
Jakub Vana <jakub.vana@embedit.cz>
2020-01-07 12:31:28 +0100

    Initial commit #2

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,14 +4,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.Simulator;
+import com.mygdx.game.constants.Settings;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.width = 1024;
-		config.height = 768;
-		config.title = "Test";
+		config.width = Settings.APP_WIDTH;
+		config.height = Settings.APP_HEIGHT;
+		config.title = Settings.APP_TITLE;
 		//config.samples = 2;
 
 		final LwjglApplication app = new LwjglApplication(new Simulator(), config);
